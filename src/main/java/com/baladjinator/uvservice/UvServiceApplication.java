a0d5cd82fef43c953bde8ccc7ac1a772dce4ae6d
Aleko Georgiev <algugone@gmail.com>
2025-03-15 12:07:54 +0200

    Add previous days request call, Remove unused files

--- src/main/java/com/baladjinator/uvservice/UvServiceApplication.java
+++ src/main/java/com/baladjinator/uvservice/UvServiceApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.client.RestTemplate;
 
 
--- src/main/java/com/baladjinator/uvservice/controllers/UvIndexController.java
+++ src/main/java/com/baladjinator/uvservice/controllers/UvIndexController.java
@@ -1,14 +1,14 @@
 package com.baladjinator.uvservice.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.cache.annotation.Cacheable;
+
+import java.time.Instant;
 import java.util.Map;
 
-@RestController("/uv")
+@RestController
+@RequestMapping("/uv")
 class UvIndexController {
 
     @Value("${openweathermap.api.url}")
@@ -23,7 +23,6 @@ class UvIndexController {
         this.restTemplate = restTemplate;
     }
 
-    @Cacheable(value = "uvIndex", key = "#city + '-current'", unless = "#result == null")
     @GetMapping("/current")
     public String getCurrentUvIndex(@RequestParam Double lat, @RequestParam Double lon) {
         var url = String.format("%s?lat=%f&lon=%f&exclude=minutely,hourly,daily,alerts&appid=%s", apiUrl, lat, lon, apiKey);
@@ -31,7 +30,20 @@ class UvIndexController {
         return response != null ? response.get("current").toString() : "UV data not found";
     }
 
-    @Cacheable(value = "uvIndex", key = "#city + '-forecast'", unless = "#result == null")
+    @GetMapping("/previous-days")
+    public String getPreviousDaysUvIndex(@RequestParam Double lat, @RequestParam Double lon, @RequestParam Short days) {
+        long currentTime = Instant.now().getEpochSecond();
+        long pastTime = currentTime - (days * 86400);
+
+        var url = String.format(
+                "%s?lat=%f&lon=%f&start=%d&end=%d&exclude=current,minutely,hourly,alerts&appid=%s",
+                apiUrl, lat, lon, pastTime, currentTime, apiKey
+        );
+
+        var response = restTemplate.getForObject(url, Map.class);
+        return response != null ? response.get("daily").toString() : "UV forecast data not found";
+    }
+
     @GetMapping("/forecast")
     public String getForecastUvIndex(@RequestParam Double lat, @RequestParam Double lon) {
         var url = String.format("%s?lat=%f&lon=%f&exclude=current,minutely,hourly,alerts&appid=%s", apiUrl, lat, lon, apiKey);
